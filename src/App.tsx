2b7743865d6b23362928056b7d715e92f87b87c1
Jessica Singh <145717016+jessica-singh79@users.noreply.github.com>
2025-10-28 20:38:12 -0400

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
 import './App.css';
 import { Hero } from './components/Hero';
+import { OurServices } from './components/OurServices'; // NEW - replaces "How It Works"
 import { ServiceCards } from './components/ServiceCards';
-import { Services } from './components/Services';
+import { AboutUs } from './components/AboutUs'; // NEW
 import { WhyChooseUs } from './components/WhyChooseUs';
 import { Testimonials } from './components/Testimonials';
 import { CTA } from './components/CTA';
+import { ContactForm } from './components/ContactForm'; // NEW
 import { Footer } from './components/Footer';
 
 function App() {
   return (
     <div className="min-h-screen overflow-x-hidden">
       <Hero />
-      <ServiceCards />
-      <Services />
+      <OurServices /> {/* Simplified - just Home & Office */}
+      <AboutUs /> {/* NEW - Your business info */}
       <WhyChooseUs />
       <Testimonials />
       <CTA />
+      <ContactForm /> {/* NEW - Contact form */}
       <Footer />
     </div>
   );
