206c192e307f3599b4b9a167379570d5e5dbe264
Jessica Singh <145717016+jessica-singh79@users.noreply.github.com>
2025-10-29 20:23:17 -0400

    Updated tsconfig.app.tsbuildinfo

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@ import './App.css';
 import { Hero } from './components/Hero';
 import { ServiceCards } from './components/ServiceCards';
 import { Services } from './components/Services';
+import { About } from './components/About';
 import { WhyChooseUs } from './components/WhyChooseUs';
 import { Testimonials } from './components/Testimonials';
+import { ContactForm } from './components/ContactForm';
 import { CTA } from './components/CTA';
 
 function App() {
@@ -12,8 +14,10 @@ function App() {
       <Hero />
       <ServiceCards />
       <Services />
+      <About />
       <WhyChooseUs />
       <Testimonials />
+      <ContactForm />
       <CTA />
     </div>
   );
